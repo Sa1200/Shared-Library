6aac3f4c8dac3fc1c5f58e257f6a57a1cd820777
Sa1200 <90265628+Sa1200@users.noreply.github.com>
2021-10-14 10:34:02 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -2,41 +2,29 @@ def call(){
   node('master') {
     def specs = [:]
     try {
-    stage('Specs Checkout'){
-      cleanWs()
-      ciFunc.checkoutVarFunc([
-      repo: Repo,
-      branch: Branch
+        stage('Code Checkout'){
+        cleanWs()
+        ciFunc.checkoutVarFunc([
+        repo: Repo,
+        branch: Branch
       ])
       stage('reading GlobalConfig & Specs'){ 
         try {
           println "reading the specs from Specs repository"
           def specsDir = "./specs/$Version"
           println "specs version" + specsDir
-            if(fileExists(specsDir + "/ci_template.yaml")){
-             ci_template = readYaml file : specsDir + "/ci_template.yaml"
-             specs = specs + ci_template
+            if(fileExists(specsDir + "/commonspecs.yml")){
+             commonspecs_template = readYaml file : specsDir + "/commonspecs.yml"
+             specs = specs + commonspecs_template
              println "reading specs file" + specs
-              }
-         
-            println "reading the global config from resources"
-            def request = libraryResource "com/hexaware/service/globalConfig/globalConfig.yaml"
-            config = readYaml text: request
-            println "reading config file" + config
             }
+        }
         catch(Exception e) {
              println "Error in reading specs file : " + e.getMessage()
         throw e
             }
       }
-     
-    stage('Code Checkout'){
-        ciFunc.checkoutVarFunc([
-        repo: specs.scm.repo,
-        branch: specs.scm.branch  
-          ])
-        }
-    }   
+      
     stage('Build'){
       ciFunc.build(specs, config)
       }
