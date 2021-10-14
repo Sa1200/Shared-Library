ca39d74382c263ad342d38036761a158f9de2796
Sa1200 <90265628+Sa1200@users.noreply.github.com>
2021-10-14 12:48:24 +0530

    Update ciFunc.groovy

--- vars/ciFunc.groovy
+++ vars/ciFunc.groovy
@@ -1,10 +1,9 @@
-def checkoutVarFunc(Map specs, Script mainScript) {
-    mainScript.checkout([$class: 'GitSCM',
-    branches: [[name: specs.branch]],
-    extensions: [],
-    userRemoteConfigs: [[url: specs.repo ]]
-    ])
-  }
+def checkoutVarFunc(Map specs) {
+  println "Printing specs" + specs
+  docheckout = new com.hexaware.service.Codecheckout(this, specs)
+  docheckout.checkOutFunc(specs)  
+}
+
 def build(Map specs, Map config) {
   dobuild = new com.hexaware.service.Build(this, specs, config)
   dobuild.buildFunc(specs, config) 
